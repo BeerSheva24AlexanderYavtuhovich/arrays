aabd4bcaa7c64cadf6e4ef679f58a31e16bdc52e
Alexander Yavtuhovich <yavtik@gmail.com>
2024-07-11 14:21:09 +0300

    added more tests

--- src/test/java/telran/util/test/ArraysTest.java
+++ src/test/java/telran/util/test/ArraysTest.java
@@ -2,6 +2,7 @@ package telran.util.test;
 
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrowsExactly;
 import org.junit.jupiter.api.Test;
 
 import static telran.util.Arrays.add;
@@ -18,28 +19,39 @@ public class ArraysTest {
         assertEquals(6, search(numbers, 14));
         assertEquals(3, search(numbers, -4));
         assertEquals(-1, search(numbers, 100));
-
     }
 
     @Test
     void addTest() {
         int newNumber = 100;
-        int[] expected = { 10, 7, 12, -4, 13, 3, 14, 100 };
+        int[] expected = { 10, 7, 12, -4, 13, 3, 14, newNumber };
         assertArrayEquals(expected, add(numbers, newNumber));
     }
 
     @Test
     void insertTest() {
-        int newNumber = 100;
-        int newIndex = 4;
-        int[] expected = { 10, 7, 12, -4, 100, 13, 3, 14 };
-        assertArrayEquals(expected, insert(numbers, newIndex, newNumber));
+        // {10, 7, 12, -4, 13, 3, 14} - all numbers
+        int newNumber = 30;
+        int[] expected_0 = { newNumber, 10, 7, 12, -4, 13, 3, 14 };
+        int[] expected_3 = { 10, 7, 12, newNumber, -4, 13, 3, 14 };
+        int[] expected_last = { 10, 7, 12, -4, 13, 3, 14, newNumber };
+        assertArrayEquals(expected_0, insert(numbers, 0, newNumber));
+        assertArrayEquals(expected_3, insert(numbers, 3, newNumber));
+        assertArrayEquals(expected_last, insert(numbers, numbers.length, newNumber));
+        assertThrowsExactly(ArrayIndexOutOfBoundsException.class, () -> insert(numbers, numbers.length + 1, newNumber));
+        assertThrowsExactly(ArrayIndexOutOfBoundsException.class, () -> insert(numbers, -1, newNumber));
     }
 
     @Test
     void removeTest() {
-        int removeIndex = 4;
-        int[] expected = { 10, 7, 12, -4, 3, 14 };
-        assertArrayEquals(expected, remove(numbers, removeIndex));
+        // {10, 7, 12, -4, 13, 3, 14} - all numbers
+        int[] expected_0 = { 7, 12, -4, 13, 3, 14 };
+        int[] expected_3 = { 10, 7, 12, 13, 3, 14 };
+        int[] expected_last = { 10, 7, 12, -4, 13, 3 };
+        assertArrayEquals(expected_0, remove(numbers, 0));
+        assertArrayEquals(expected_3, remove(numbers, 3));
+        assertArrayEquals(expected_last, remove(numbers, numbers.length - 1));
+        assertThrowsExactly(ArrayIndexOutOfBoundsException.class, () -> remove(numbers, numbers.length));
+        assertThrowsExactly(ArrayIndexOutOfBoundsException.class, () -> remove(numbers, -1));
     }
 }
